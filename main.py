9295143333df0d4de6c9e07d01550abd5b721f45
rootamin <osalimohammadamin@gmail.com>
2024-10-04 14:08:56 +0330

    completed the function for uploading to telegram and then deleting the uploaded files

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import os
 import subprocess
 from dotenv import load_dotenv
 
+import asyncio
+from telegram import Bot
+from telegram.error import TelegramError
+
 # Looks for the .env file in the same directory as this file
 load_dotenv()
 
 # Variables:
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+MEDIA_DIR = os.getenv('MEDIA_DIR')
 
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
@@ -14,6 +20,8 @@ DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+CHAT_ID = os.getenv('CHAT_ID')
 
 
 # Function to rar a directory
@@ -24,6 +32,8 @@ def rar_directory(directory_path, output_path, part_size=49):
 # rar_directory('/home/rootamin/Downloads/mpv-build', f'{BASE_DIR}/backups/backup.rar')
 
 
+
+# A function to dump the PostgreSQL database
 def get_db_dump(output_path):
     try:
         # Construct the pg_dump command
@@ -51,5 +61,29 @@ def get_db_dump(output_path):
         print(f"Error occurred while dumping the database: {e}")
 
 # Example usage
-output_file = os.path.join(BASE_DIR, 'backups', 'postgres.dumpfile')
-get_db_dump(output_file)
+# output_file = os.path.join(BASE_DIR, 'backups', 'postgres.dumpfile')
+# get_db_dump(output_file)
+
+
+
+# Telegram Section
+bot = Bot(token=TELEGRAM_BOT_TOKEN)
+
+# Function to send and delete files in a directory. Use it with asyncio.run()
+async def send_and_delete_files(directory_path):
+    try:
+        for root, dirs, files in os.walk(directory_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                with open(file_path, 'rb') as f:
+                    await bot.send_document(chat_id=CHAT_ID, document=f)
+                    print(f"Sent {file_path} to Telegram chat {CHAT_ID}")
+                os.remove(file_path)
+                print(f"Deleted {file_path}")
+    except TelegramError as e:
+        print(f"Error occurred while sending file: {e}")
+    except OSError as e:
+        print(f"Error occurred while deleting file: {e}")
+
+# Example usage
+# asyncio.run(send_and_delete_files('/home/rootamin/Downloads/YekanV3.0'))
